c8b563f20db9edab00958e4872a1506b8fb176c6
Todd Sedano <todd.sedano@sv.cmu.edu>
2013-04-19 19:35:34 -0700

    Update and rename symmetry.rb to symmetry_before.rb

--- symmetry_before.rb
+++ symmetry_before.rb
@@ -14,11 +14,3 @@
 		end
 	end
 
-#After
-  def self.move_when_facing_north move
-		turn_east if move == "r"
-		turn_west if move == "l"
-		increase_y_coordinate if move == "f"
-		decrease_y_coordinate if move == "b"
-	end
-
