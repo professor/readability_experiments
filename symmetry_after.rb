fd1ea7f7164e6cd0b01e6b042a184654a0f86b0e
Todd Sedano <todd.sedano@sv.cmu.edu>
2013-04-19 19:35:54 -0700

    Create symmetry_after.rb

---
+++ symmetry_after.rb
@@ -0,0 +1,8 @@
+#After
+  def self.move_when_facing_north move
+  	turn_east if move == "r"
+		turn_west if move == "l"
+		increase_y_coordinate if move == "f"
+		decrease_y_coordinate if move == "b"
+	end
+
